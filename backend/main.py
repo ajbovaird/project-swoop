2b2821ce40386a9a2df93aae304fc86add68ae6d
AJ Bovaird <aj.bovaird@uplight.com>
2022-11-15 19:21:22 -0500

    Containerize backend

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,9 @@
 from fastapi import FastAPI, APIRouter
 
-app = FastAPI(
-    title="Project Swoop", openapi_url="/openapi.json"
-)
+app = FastAPI(title="Project Swoop", openapi_url="/openapi.json")
 
 api_router = APIRouter()
+
 
 @api_router.get("/", status_code=200)
 def root() -> dict:
@@ -13,8 +12,10 @@ def root() -> dict:
     """
     return {"msg": "Hello, World!"}
 
+
 app.include_router(api_router)
 
 if __name__ == "__main__":
     import uvicorn
+
     uvicorn.run(app, host="0.0.0.0", port=8001, log_level="debug")
---
+++ justfile
@@ -0,0 +1,8 @@
+build *build_args:
+    docker compose build {{build_args}}
+
+up:
+    docker compose up
+
+down:
+    docker compose down
